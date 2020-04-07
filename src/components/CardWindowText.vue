5630aa6c5045de28115e235437fdf3da59e23a01
Reuter Junior <reuter_junior@hotmail.com>
2020-04-07 00:21:18 -0300

    adicionado grid flexbox para ficar lado a lado com a componente de carousel

---
+++ src/components/CardWindowText.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-col sm="6">
+    <v-card>
+      <v-row dense>
+        <v-col
+          cols="12"
+          sm="12"
+          elevation="6"
+          v-for="(item,i) in items"
+          :key="i"
+        >
+          <v-card dark>
+            <div class="d-flex flex-no-wrap justify-space-between">
+              <div>
+                <v-card-title class="justify-center" v-text="item.title"/>
+                <v-card-subtitle v-text="item.text"/>
+              </div>
+            </div>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-col>
+</template>
+
+<script>
+export default {
+  name: 'CardWindowText',
+  data: () => ({
+    items: [
+      {
+        title: 'Serviço Premium',
+        text: 'Serviços de alta qualidade com transparencia em todas as transações'
+      },
+      {
+        title: 'Taxa?',
+        text: 'Sem taxas adicionais ou qualquer outra tarifa nas suas transações'
+      }
+    ]
+  })
+}
+</script>
+
+<style>
+
+</style>
