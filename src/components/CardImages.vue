c8b21274ecd9733f4e5c3928a6b05cc06e50e658
Reuter Junior <reuter_junior@hotmail.com>
2020-04-07 00:22:49 -0300

    adicionado carousel e grid flexbox

--- src/components/CardImages.vue
+++ src/components/CardImages.vue
@@ -1,11 +1,21 @@
 <template>
-  <v-col cols="12" sm="6">
-    <v-card elevation="6">
-      <v-card-title primary-title class="justify-center"/> TEST
-      <v-divider class="mx-4"/>
-      <v-carousel cycle heigth="400" hide-delimiter-backgroud show-arrows-on-hover>
+  <v-col sm="6" elevation="6" cols="12">
+    <v-card>
+        <!-- TODO: Adicionar 'show-arrows' e tentar resolver bug de ambos 'prev/next'
+                   estarem no mesmo lado
+        -->
+      <v-carousel
+        class="mx-auto"
+        cycle
+        :show-arrows=false
+        interval="3000"
+      >
         <!-- TODO: Adicionar imagens quando terminar o prototipo das telas -->
-        <v-carousel-item>
+        <v-carousel-item
+          v-for="(item,i) in items"
+          :key="i"
+          :src="item.src"
+        >
         </v-carousel-item>
       </v-carousel>
     </v-card>
@@ -14,7 +24,15 @@
 
 <script>
 export default {
-  name: 'CardImages'
+  name: 'CardImages',
+  data: () => ({
+    items: [
+      { src: '@/assets/img/img1.jpg' },
+      { src: '@/assets/img/2.jpg' },
+      { src: '@/assets/img/3.jpg' },
+      { src: '@/assets/img/4.jpg' }
+    ]
+  })
 }
 </script>
 
